ced6d2a676c74e54a933ac47e7e68816b2393c66
Jemila <jemila.abulhawa@gmail.com>
2019-07-02 17:12:51 +0300

    natours 2: GET api/v1/tours without comments

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -3,21 +3,14 @@ const app = express()
 
 const fs = require('fs')
 
-// 2 - readFileSync
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 )
 
-// we should always specfiy the version of the API. You can also do it in the subdomain.
-// route handler
-// tours is the resource
 app.get('/api/v1/tours', (req, res) => {
   res.json({
-    // using jssend specification
     status: 'success',
-    // include result number when response is an array - makes it easy for the client to get key information
     results: tours.length,
-    // envelope for our data
     data: { tours }
   })
 })
