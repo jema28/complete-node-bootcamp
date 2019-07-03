bd9a861430de369a1db815ac69ebb556687d39be
Jemila <jemila.abulhawa@gmail.com>
2019-07-03 10:15:30 +0300

    natours 3: get tours, post/get tour w/o comments

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -3,7 +3,6 @@ const fs = require('fs')
 
 const app = express()
 
-// middleware is a function that can modify the incoming request data - stands in the middle of the request and the response
 app.use(express.json())
 
 const tours = JSON.parse(
@@ -18,15 +17,10 @@ app.get('/api/v1/tours', (req, res) => {
   })
 })
 
-// variable starting with colon
 app.get('/api/v1/tours/:id', (req, res) => {
-  // req.params is the place where all the variables that we define are stored (parameters) - can do multiple
-  // can create an optional param by adding ? after the variable `:y?` (maybe could be before)
   const id = req.params.id * 1
   const tour = tours.find(tour => tour.id === id)
 
-  // can't find id for that tour
-  // if (id > tours.length)
   if (!tour) {
     return res.status(404).json({
       status: 'fail',
@@ -40,21 +34,17 @@ app.get('/api/v1/tours/:id', (req, res) => {
   })
 })
 
-// the way we send data from the client to the server. This data is available on the request. The request object holds all the information about the request including data that was sent. In order to have get this data, we have to use something called middleware.
 app.post('/api/v1/tours', (req, res) => {
   console.log('req.body', req.body)
   const newId = tours[tours.length - 1].id + 1
-  // create a new object by merging two objects existing objects together
   const newTour = Object.assign({ id: newId }, req.body)
   console.log('newTour', newTour)
   tours.push(newTour)
 
-  // now we want to persist this data into our simple JSON tour file
   fs.writeFile(
     `${__dirname}/dev-data/data/tours-simple.json`,
     JSON.stringify(tours),
     err => {
-      // status 201 stands for created - created a new resource on the server
       res.status(201).json({
         status: 'success',
         data: {
