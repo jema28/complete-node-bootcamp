1115f0ef43e1e09a96c09c24b61482769cc08ab1
Jemila <jemila.abulhawa@gmail.com>
2019-07-03 12:01:03 +0300

    natours 5: adding own middleware w/o comments

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -3,13 +3,10 @@ const fs = require('fs')
 
 const app = express()
 
-// this returns a function that's added to the middleware stack.
 app.use(express.json())
 
-// next is the third argument of the middleware function
 app.use((req, res, next) => {
   console.log('hello from the middleware')
-  // without next, the program will be stuck in the middleware stack
   next()
 })
 
