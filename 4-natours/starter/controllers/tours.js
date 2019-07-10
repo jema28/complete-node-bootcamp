cb7404f953617b22d694bf8e7bb6ca77b4a22496
Jemila <jemila.abulhawa@gmail.com>
2019-07-10 12:01:59 +0300

    natours 16.5: remove comments / tidy up

--- 4-natours/starter/controllers/tours.js
+++ 4-natours/starter/controllers/tours.js
@@ -2,7 +2,6 @@ const Tour = require('../models/tours')
 
 exports.getAllTours = async (req, res) => {
   try {
-    // converts the result to JS objects
     const tours = await Tour.find()
 
     res.status(200).json({
@@ -20,7 +19,6 @@ exports.getAllTours = async (req, res) => {
 
 exports.getTour = async (req, res) => {
   try {
-    // shorthand for Tour.findOne({_id: req.params.id})
     const tour = await Tour.findById(req.params.id)
 
     res.status(200).json({
@@ -36,12 +34,6 @@ exports.getTour = async (req, res) => {
 }
 
 exports.createTour = async (req, res) => {
-  // another way of creating a new instance
-  // use the Tour model directly and call the create method on it.
-  // in that function we pass the data we want to store in the database (from the request.body)
-  // this create method returns a promise which we save in async await / store this in newTour variable
-  //
-  // we have a try catch because we're using an async await function
   try {
     const newTour = await Tour.create(req.body)
 
@@ -54,7 +46,6 @@ exports.createTour = async (req, res) => {
   } catch (err) {
     res.status(400).json({
       status: 'fail',
-      // using this for now but will do proper error handling later
       message: 'Invalid data sent'
     })
   }
