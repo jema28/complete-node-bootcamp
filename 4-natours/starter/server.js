3d6f7899c7e0b40e7d78b7f9031957548c44be38
Jemila <jemila.abulhawa@gmail.com>
2019-07-10 11:11:21 +0300

    natours 13: set up mongoose - connection - first schema - run eslint

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,7 +1,47 @@
+const mongoose = require('mongoose')
 const env = require('dotenv')
+const app = require('./app')
+
 env.config({ path: './config.env' })
 
-const app = require('./app')
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+)
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() => console.log('DB connection successful'))
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    // validator
+    required: [true, 'Name is required'],
+    unique: true
+  },
+  rating: Number,
+  price: {
+    type: Number,
+    require: [true, 'A tour must have a price']
+  }
+})
+
+// convention of using model names and variables
+const Tour = mongoose.model('Tour', tourSchema)
+
+const testTour = new Tour({
+  name: 'The Park Camper',
+  price: 997
+})
+
+testTour
+  .save()
+  .then(document => console.log(document))
+  .catch(error => console.log(error))
 
 const port = process.env.PORT || 3000
 
