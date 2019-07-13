cd1dd02855bf48d37021aa5b4c40299007004f64
Jemila <jemila.abulhawa@gmail.com>
2019-07-13 18:38:11 +0300

    natours 19: tour model

--- 4-natours/starter/models/tours.js
+++ 4-natours/starter/models/tours.js
@@ -4,13 +4,52 @@ const tourSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, 'Name is required'],
-    unique: true
+    unique: true,
+    trim: true
   },
-  rating: Number,
+  duration: {
+    type: Number,
+    required: [true, 'A tour must have a duration']
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, 'A tour must have a group size']
+  },
+  difficulty: {
+    type: String,
+    required: [true, 'A tour must have a difficulty']
+  },
+  ratingsAverage: {
+    type: Number,
+    default: 4.5
+  },
+  ratingsQuantity: {
+    type: Number,
+    default: 0
+  },
   price: {
     type: Number,
     require: [true, 'A tour must have a price']
-  }
+  },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    trim: true
+  },
+  description: {
+    type: String,
+    trim: true,
+    required: [true, 'A tour must have a description']
+  },
+  imageCover: {
+    type: String
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  },
+  startDates: [Date]
 })
 
 const Tour = mongoose.model('Tour', tourSchema)
