7c88ad42fe8b0ac08d05a5b5f7bb1e025957466c
Jemila <jemila.abulhawa@gmail.com>
2019-07-21 10:35:47 +0300

    natours 39: errors during development / production

--- 4-natours/starter/controllers/error.js
+++ 4-natours/starter/controllers/error.js
@@ -1,8 +1,36 @@
+const sendErrorDevelopment = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack
+  })
+}
+
+const sendErrorProduction = (err, res) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message
+    })
+  } else {
+    // eslint-disable-next-line no-console
+    console.log('ERROR 🔥', err)
+
+    res.status(500).json({
+      status: 'error',
+      message: 'Oops, something went wrong'
+    })
+  }
+}
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500
   err.status = err.status || 'error'
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message
-  })
+
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDevelopment(err, res)
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProduction(err, res)
+  }
 }
