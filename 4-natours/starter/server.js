c82152a9fa6e61fc9f87622c05c1a5bdc1833633
Jemila <jemila.abulhawa@gmail.com>
2019-07-21 11:44:12 +0300

    natours 44: handling uncaught exceptions

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -3,6 +3,12 @@
 const mongoose = require('mongoose')
 const env = require('dotenv')
 
+process.on('uncaughtException', err => {
+  console.log('Unhandled exception. Shutting down.')
+  console.log(err.name, err.message)
+  process.exit(1)
+})
+
 env.config({ path: './config.env' })
 const app = require('./app')
 
@@ -25,8 +31,8 @@ const server = app.listen(port, () => {
 })
 
 process.on('unhandledRejection', err => {
-  console.log(err.name, err.message)
   console.log('Unhandled rejection. Shutting down.')
+  console.log(err.name, err.message)
   server.close(() => {
     process.exit(1)
   })
