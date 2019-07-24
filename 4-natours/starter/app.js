7caea71655997c04cd69c78918bff0f947676ee5
Jemila <jemila.abulhawa@gmail.com>
2019-07-24 18:48:37 +0300

    natours 10-144 : add data sanitisation

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -2,6 +2,10 @@ const express = require('express')
 const morgan = require('morgan')
 const rateLimit = require('express-rate-limit')
 const helmet = require('helmet')
+const mongoSanitise = require('express-mongo-sanitize')
+const xss = require('xss-clean')
+const hpp = require('hpp')
+
 const AppError = require('./utils/appError')
 const globalErrorHandler = require('./controllers/error')
 const tourRouter = require('./routes/tours')
@@ -28,6 +32,14 @@ app.use('/api', limiter)
 
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }))
+
+// Data santisation against NoSQL query injections
+// will remove dollar signs / operator signs
+app.use(mongoSanitise())
+
+// Data santisation against XSS
+// stops you adding html code / converts html entities
+app.use(xss())
 
 // Serving static files
 app.use(express.static(`${__dirname}/public`))
