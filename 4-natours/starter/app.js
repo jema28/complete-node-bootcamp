d38c8a83c75f2f4c743b9eabad85e92c9050b89e
Jemila <jemila.abulhawa@gmail.com>
2019-07-24 18:24:13 +0300

    natours 10-142 : implement rate limiting

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
-
+const rateLimit = require('express-rate-limit')
 const AppError = require('./utils/appError')
 const globalErrorHandler = require('./controllers/error')
 const tourRouter = require('./routes/tours')
@@ -12,6 +12,13 @@ const app = express()
 if (process.env.NODE_ENV) {
   app.use(morgan('dev'))
 }
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour.'
+})
+app.use('/api', limiter)
 
 app.use(express.json())
 app.use(express.static(`${__dirname}/public`))
