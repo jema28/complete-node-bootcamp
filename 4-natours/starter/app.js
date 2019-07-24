ea62fc938c9c76a11b054309a0cc874a76f00a05
Jemila <jemila.abulhawa@gmail.com>
2019-07-24 18:33:07 +0300

    natours 10-143 : setting secruity http headers - add comments to each middleware in the the stack

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const morgan = require('morgan')
 const rateLimit = require('express-rate-limit')
+const helmet = require('helmet')
 const AppError = require('./utils/appError')
 const globalErrorHandler = require('./controllers/error')
 const tourRouter = require('./routes/tours')
@@ -9,10 +10,15 @@ const userRouter = require('./routes/users')
 // --- MIDDLEWARES ---
 const app = express()
 
-if (process.env.NODE_ENV) {
+// Set secruity HTTP headers
+app.use(helmet())
+
+// Development logging
+if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
 }
 
+// Limit request from same API
 const limiter = rateLimit({
   max: 100,
   windowMs: 60 * 60 * 1000,
@@ -20,7 +26,10 @@ const limiter = rateLimit({
 })
 app.use('/api', limiter)
 
-app.use(express.json())
+// Body parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' }))
+
+// Serving static files
 app.use(express.static(`${__dirname}/public`))
 
 // -- ROUTERS ---
