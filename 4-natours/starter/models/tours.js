439bb42fe887290a8560a0fb06851785b6334cf0
Jemila <jemila.abulhawa@gmail.com>
2019-07-25 11:17:16 +0300

    natours 11-150 - model tour guides: embedding

--- 4-natours/starter/models/tours.js
+++ 4-natours/starter/models/tours.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const slugify = require('slugify')
+const User = require('./users')
 
 const tourSchema = new mongoose.Schema(
   {
@@ -97,7 +98,8 @@ const tourSchema = new mongoose.Schema(
         description: String,
         day: Number
       }
-    ]
+    ],
+    guides: Array
   },
   {
     toJSON: { virtuals: true },
@@ -112,6 +114,11 @@ tourSchema.virtual('durationWeeks').get(function() {
 tourSchema.pre('save', function(next) {
   this.slug = slugify(this.name, { lower: true })
   next()
+})
+
+tourSchema.pre('save', async function(next) {
+  const guidesPromises = this.guides.map(async id => await User.findById(id))
+  this.guides = await Promise.all(guidesPromises)
 })
 
 // Query middleware
