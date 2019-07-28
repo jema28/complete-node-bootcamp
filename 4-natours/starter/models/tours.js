1418dab0e2d28fa46900d301e537dd55926ede9c
Jemila <jemila.abulhawa@gmail.com>
2019-07-28 10:12:59 +0300

    natours 11-152 : populating tour guides

--- 4-natours/starter/models/tours.js
+++ 4-natours/starter/models/tours.js
@@ -129,6 +129,14 @@ tourSchema.pre(/^find/, function(next) {
   next()
 })
 
+tourSchema.pre(/^find/, function(next) {
+  this.populate({
+    path: 'guides',
+    select: '-__v -passwordChangedAt'
+  })
+  next()
+})
+
 tourSchema.post(/^find/, function(documents, next) {
   // eslint-disable-next-line no-console
   console.log(`Query took ${Date.now() - this.start} milliseconds!`)
